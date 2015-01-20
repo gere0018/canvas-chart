b1b1ad4c7d9d12a54b0475c074bc7642c7303004
Nehmat <gere0018@algonquinlive.com>
2015-01-20 15:53:49 -0500

    Got maxvalue and minvalue in pie chart and changed the radius for them.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,9 @@ var Value;
 var color;
 //upon loading document, make Ajax call to retrieve data and add event listener to buttons.
 document.addEventListener("DOMContentLoaded", function () {
- addButtonListeners();
+
+  addButtonListeners();
+
  var request = $.ajax({
   url: "js/cheese.json",
   type: "GET",
@@ -37,10 +39,21 @@ function showPie(){
   context = canvas.getContext("2d");
     //clear canvas if there is any previous inputs.
   context.clearRect(0, 0, canvas.width, canvas.height);
-  var cx = (canvas.width/2) - 40;
-  var cy = canvas.height/2 ;
-  var radius = 110;
-  var currentAngle = 0;
+   var cx = (canvas.width/2) - 40;
+   var cy = canvas.height/2 ;
+   var radius = 110;
+   var currentAngle = 0;
+   var maxValue = Value[0].value;
+   var minValue = Value[0].value;
+   for(var i=0; i<Value.length; i++){
+      if(maxValue < Value[i].value){
+        maxValue = Value[i].value;
+      }
+      if(minValue > Value[i].value){
+        minValue = Value[i].value;
+      }
+   }
+
   for(var i=0; i<Value.length; i++){
     percentage = Value[i].value/total;
     color = Value[i].color;
@@ -48,7 +61,14 @@ function showPie(){
     context.moveTo(cx, cy);
     context.beginPath();
     context.fillStyle = color;
-    context.arc(cx, cy, radius, currentAngle, endAngle, false);
+      if(Value[i].value == maxValue){
+        context.arc(cx, cy, radius*90/100, currentAngle, endAngle, false);
+      }else if(Value[i].value == minValue){
+        context.arc(cx, cy, radius*110/100, currentAngle, endAngle, false);
+      }else{
+        context.arc(cx, cy, radius, currentAngle, endAngle, false);
+      }
+
     context.lineTo(cx, cy);
     context.fill();
     //The lines that will point to the values
